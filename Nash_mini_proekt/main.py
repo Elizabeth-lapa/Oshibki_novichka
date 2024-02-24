f8938aa980aa2c7486b9a80e3d8f6fa2fd150313
aslitvinenko <oven034@mail.ru>
2024-02-24 18:56:01 +0300

    Красивее

--- Nash_mini_proekt/main.py
+++ Nash_mini_proekt/main.py
@@ -4,11 +4,11 @@ from prost import prost
 
 class ProstTestCase(unittest.TestCase):
     def test_prost(self):
-        self.assertEqual(prost(7), True)
-        self.assertEqual(prost(123), False)
-        self.assertEqual(prost(67), True)
-        self.assertEqual(prost(68), False)
-        self.assertEqual(prost(90), False)
+        self.assertEqual(prost(7), 'Да')
+        self.assertEqual(prost(123), 'Нет')
+        self.assertEqual(prost(67), 'Да')
+        self.assertEqual(prost(68), 'Нет')
+        self.assertEqual(prost(90), 'Нет')
 
 
 if __name__ == '__main__':
--- Nash_mini_proekt/prost.py
+++ Nash_mini_proekt/prost.py
@@ -1,9 +1,18 @@
 def prost(n):
     for i in range(2, n - 1):
         if n % i == 0:
-            return False
-    return True
+            print(f'Число {n} делится на', i)
+            return 'Нет'
+    return 'Да'
 
 
+print('Введите число для проверки на простоту (0 для выхода): ', end='')
 n = int(input())
-print("Число простое?", prost(n))
+while n != 0:
+    x = prost(n)
+    if x == 'Нет':
+        print("Число простое?", x)
+    else:
+        print("Число простое?", x)
+    print('Введите число для проверки на простоту (0 для выхода): ', end='')
+    n = int(input())
