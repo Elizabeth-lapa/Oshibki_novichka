463ce0a35709c9a5f287f4fb97a7f3fcc25ae84e
O-D-V <dmitry712543@gmail.com>
2024-03-15 22:23:14 +0300

    calendarBot

---
+++ GulEndar/CalendarBot/src/main/java/Calendar/CalendarBot/body/TelegramBotBody.java
@@ -0,0 +1,63 @@
+package Calendar.CalendarBot.body;
+
+import Calendar.CalendarBot.config.BotConfig;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+public class TelegramBotBody extends TelegramLongPollingBot {
+    private final BotConfig botConfig;
+
+    public TelegramBotBody(BotConfig botConfig){
+        this.botConfig = botConfig;
+    }
+    @Override
+    public String getBotUsername() {
+        return botConfig.getBotName();
+    }
+
+    @Override
+    public String getBotToken() {
+        return botConfig.getToken();
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        String currency = "";
+
+        if(update.hasMessage() && update.getMessage().hasText()){
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+
+            switch (messageText){
+                case "/start":
+                    startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
+                    break;
+                default:
+
+                    sendMessage(chatId, currency);
+            }
+        }
+
+    }
+
+    private void startCommandReceived(Long chatId, String name) {
+        String answer = "Hi, " + name + ", nice to meet you!";
+        sendMessage(chatId, answer);
+    }
+
+    private void sendMessage(Long chatId, String textToSend){
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(String.valueOf(chatId));
+        sendMessage.setText(textToSend);
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+
+        }
+    }
+}
+
---
+++ GulEndar/CalendarBot/src/test/java/Calendar/CalendarBot/CalendarBotApplicationTests.java
@@ -0,0 +1,13 @@
+package Calendar.CalendarBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
